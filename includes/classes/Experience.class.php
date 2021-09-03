709316941c092243f969c8b86fe93146e2816929
Johannes Nilsson <johannes@nilsson.im>
2021-09-03 14:34:29 +0200

    Add getters

--- includes/classes/Experience.class.php
+++ includes/classes/Experience.class.php
@@ -57,9 +57,99 @@ class Experience {
       // Execute statement
       if($stmt->execute()) {
          return true;
+
+         // TODO: Here I should return the data of the created item.
       }
 
       // If anything fails, return false
       return false;
+   }
+
+   public function getAll() {
+      // Set up the query
+      $query = 'SELECT * FROM' . $this->db_table;
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Create array that can be sent as response
+      $response = array();
+
+      // Execute statement
+      if($stmt->execute()) {
+         // Count the results
+         $count = $stmt->rowCount();
+
+         // Nested array that will hold the actual data
+         $response['data'] = array();
+
+         // Save the amount of results
+         $data['itemCount'] = $count;
+
+         // If there are results, store them in $response['data']
+         if($count > 0) {
+            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+               extract($row);
+
+               $item = array(
+                  'id' => $id,
+                  'title' => $title,
+                  'type' => $type,
+                  'location' => $location,
+                  'description' => $description,
+                  'date_start' => $date_start,
+                  'date_end' => $date_end,
+                  'created' => $created,
+               );
+
+               array_push($response['data'], $item);
+            }
+         }
+      }
+
+      // Return response
+      /* TODO: This should be written
+       * in a way so that the user can get both
+       * a success response or a failed one..
+       */
+      return $response;
+   }
+
+   public function getOne($id) {
+      // Set up the query
+      $query = 'SELECT * FROM
+                  ' . $this->db_table . '
+               WHERE
+                  id = :id
+               LIMIT 1';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Bind input data to param
+      $stmt->bindParam(':id', $id);
+
+      // Create array that can be sent as response
+      $response = array();
+
+      // Execute statement
+      if($stmt->execute()) {
+         // Count the results
+         $count = $stmt->rowCount();
+
+         // If there are results, store them in $response['data']
+         if($count > 0) {
+            $response['data'] = $stmt->fetch(PDO::FETCH_ASSOC);
+         } else {
+            $response['data'] = array();
+         }
+      }
+      
+      // Return response
+      /* TODO: This should be written
+       * in a way so that the user can get both
+       * a success response or a failed one..
+       */
+      return $response;
    }
 }
